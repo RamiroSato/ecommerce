71a3b1692f5399a0de6daccc54df2b06b9751ada
r.sato <satoramirodev@gmail.com>
2025-01-27 23:51:15 -0300

    Add ItemPreOrden and PreOrden classes to Ecommerce.Models
    Introduce ItemPreOrden and PreOrden classes with relevant properties
    to represent items and pre-orders in the Ecommerce.Models namespace.
    Include necessary using directives for common namespaces.

---
+++ Ecommerce.Models/ItemPreOrden.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ID: GUID
+IdLote: GUID
+IdPreOrden: GUID
+PrecioUnitario: decimal
+Cantidad: Int
+Activo: bool
+FechaAlta: DateTime
+*/
+
+namespace Ecommerce.Models;
+
+public class ItemPreOrden
+{
+    public Guid ID { get; set; }
+    public Guid IdLote { get; set; }
+    public Guid IdPreOrden { get; set; }
+    public decimal PrecioUnitario { get; set; }
+    public int Cantidad { get; set; }
+    public bool Activo { get; set; }
+    public DateTime FechaAlta { get; set; }
+}
---
+++ Ecommerce.Models/PreOrden.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    IdUsuario: GUID
+    Descripcion: string
+    Activo: bool
+    Vencimiento: DateTime
+    FechaAlta: DateTime
+*/
+
+namespace Ecommerce.Models;
+
+public class PreOrden
+{
+    public Guid IdUsuario { get; set; }
+    public string Descripcion { get; set; }
+    public bool Activo { get; set; }
+    public DateTime Vencimiento { get; set; }
+    public DateTime FechaAlta { get; set; }
+    public List<ItemPreOrden> Items { get; set; }
+}
